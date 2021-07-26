7b1a2adc798bd9dd53f38142d19c918a70f3e373
KenInoue1911 <inouetkrk3@gamail.com>
2021-07-26 21:41:37 +0900

    7/26

---
+++ paiza/paiza入門2/おみくじ.php
@@ -0,0 +1,9 @@
+<?php
+$omikuji = rand(1,100);
+if($omikuji >= 29){
+  echo $omikuji."なので大吉";
+
+}else{
+  echo $omikuji."なので大凶";
+}
+ ?>
---
+++ paiza/paiza入門2/比較演算子で賢い条件分岐3.php
@@ -0,0 +1,10 @@
+<?php
+$age = rand(15,25);
+echo "ageの中身:".$age."\n";
+if($age >= 20){
+  echo $age."才成人です。";
+
+}else{
+  echo $age."才未成人です。";
+}
+?>
